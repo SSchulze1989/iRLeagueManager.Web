d22dac75f3fc2427935e395432527832bf07523f
Simon Schulze <simonschulze2589@hotmail.de>
2022-05-12 20:29:25 +0200

    Add session data fields

--- iRLeagueManager.Web/ViewModels/SessionViewModel.cs
+++ iRLeagueManager.Web/ViewModels/SessionViewModel.cs
@@ -22,6 +22,18 @@ namespace iRLeagueManager.Web.ViewModels
 
         public long SessionId { get => model.SessionId; set => SetP(model.SessionId, value => model.SessionId = value, value); }
         public string Name { get => model.Name; set => SetP(model.Name, value => model.Name = value, value); }
+        public DateTime Date 
+        { 
+            get => model.Date.GetValueOrDefault().Date; 
+            set => SetP(model.Date, value => model.Date = value.GetValueOrDefault().Add(model.Date.GetValueOrDefault().TimeOfDay), value); 
+        }
+        public long? TrackId { get => model.TrackId; set => SetP(model.TrackId, value => model.TrackId = value, value); }
+        public int Laps { get => model.Laps; set => SetP(model.Laps, value => model.Laps = value, value); }
+        public TimeSpan SessionStart 
+        { 
+            get => model.Date.GetValueOrDefault().TimeOfDay;
+            set => SetP(model.Date.GetValueOrDefault().TimeOfDay, value => model.Date = model.Date.GetValueOrDefault().Date.Add(value), value); 
+        }
 
         public void SetModel(GetSessionModel model)
         {

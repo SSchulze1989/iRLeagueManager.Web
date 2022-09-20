52599c303d5f97dc932dc5d6e7ffb85617f9ad51
Simon Schulze <simonschulze2589@hotmail.de>
2022-09-20 19:41:06 +0200

    Update EditEventModal

--- src/iRLeagueManager.Web/ViewModels/EventViewModel.cs
+++ src/iRLeagueManager.Web/ViewModels/EventViewModel.cs
@@ -156,6 +156,7 @@ namespace iRLeagueManager.Web.ViewModels
                         model.Sessions.Remove(sessionModel);
                     }
                 }
+                ReorderSesssionNrs();
                 Loading = true;
                 var result = await ApiService.CurrentLeague
                     .Events()
@@ -169,28 +170,129 @@ namespace iRLeagueManager.Web.ViewModels
             }
         }
 
-        private SessionViewModel AddSession(string name, SessionType sessionType, bool onlyOnce=false)
+        private void ReorderSesssionNrs()
         {
-            // if onlyOnce is true check if a session of that type exists inside model
-            SessionModel sessionModel = default!;
-            if (onlyOnce)
+            // find practice session and move first:
+            int currentIndex = 1;
+            var practice = Sessions.FirstOrDefault(x => x.SessionType == SessionType.Practice);
+            if (practice != null)
             {
+                practice.SessionNr = currentIndex++;
+            }
+            // find qualifying and move up (behind practice)
+            var qualifying = Sessions.FirstOrDefault(x => x.SessionType == SessionType.Qualifying);
+            if (qualifying != null)
+            {
+                qualifying.SessionNr = currentIndex++;
+            }
+            // sort those sessions that have a number and make them ascending +1 each
+            foreach(var session in Sessions.Except(new[] {practice, qualifying}).Where(x => x?.SessionNr != 0).OrderBy(x => x!.SessionNr))
+            {
+                session!.SessionNr = currentIndex++;
+            }
+            // cover the rest
+            foreach(var session in Sessions.Where(x => x?.SessionNr == 0))
+            {
+                session!.SessionNr = currentIndex++;
+            }
+        }
+
+        private void SortSessions()
+        {
+            Sessions = new ObservableCollection<SessionViewModel>(Sessions.OrderBy(x => x.SessionNr));
+        }
+
+        public SessionViewModel AddSession(string name, SessionType sessionType, bool onlyOnce=false)
+        {
+            SessionModel sessionModel = default!;
+            bool reorderNumbers = false;
+            if (onlyOnce)
+            {   
+                // if onlyOnce is true check if a session of that type exists inside model
                 sessionModel = model.Sessions
                     .FirstOrDefault(x => x.SessionType == sessionType)!;
             }
             if (sessionModel == null)
             {
+                // find the first session model in model.Sessions that is not currently part of a SesionViewModel
+                // and that matches the requested type
+                sessionModel = model.Sessions
+                    .Where(x => Sessions.Any(y => y.GetModel() == x) == false)
+                    .Where(x => x.SessionType == sessionType)
+                    .FirstOrDefault()!;
+            }
+            if (sessionModel == null)
+            {
+                // When no suitable model exists, create new
                 sessionModel = new SessionModel()
                 {
                     Name = name,
                     SessionType = sessionType,
                 };
                 model.Sessions.Add(sessionModel);
+                reorderNumbers = true;
             }
 
             var session = new SessionViewModel(LoggerFactory, ApiService, sessionModel);
             Sessions.Add(session);
+            if (reorderNumbers)
+            {
+                ReorderSesssionNrs();
+            }
+            SortSessions();
             return session;
+        }
+
+        public bool CanMoveSessionUp(SessionViewModel session)
+        {
+            var sessionIndex = Sessions.IndexOf(session);
+            return sessionIndex > 0 && Sessions.ElementAt(sessionIndex - 1).SessionType == SessionType.Race;
+        }
+
+        public bool CanMoveSessionDown(SessionViewModel session)
+        {
+            var sessionIndex = Sessions.IndexOf(session);
+            return sessionIndex < Sessions.Count -1 && Sessions.ElementAt(sessionIndex + 1).SessionType == SessionType.Race;
+        }
+
+        public void MoveSessionUp(SessionViewModel session)
+        {
+            // find previous session that is no a practice or qualy
+            var sessionIndex = Sessions.IndexOf(session);
+            if (sessionIndex <= 0)
+            {
+                return;
+            }
+            var prevSession = Sessions.ElementAt(sessionIndex - 1);
+            if (prevSession.SessionType != SessionType.Race)
+            {
+                return;
+            }
+            // swap sessions
+            var tmp = session.SessionNr;
+            session.SessionNr = prevSession.SessionNr;
+            prevSession.SessionNr = tmp;
+            SortSessions();
+        }
+
+        public void MoveSessionDown(SessionViewModel session)
+        {
+            // find next session that is no a practice or qualy
+            var sessionIndex = Sessions.IndexOf(session);
+            if (sessionIndex >= Sessions.Count)
+            {
+                return;
+            }
+            var nextSession = Sessions.ElementAt(sessionIndex + 1);
+            if (nextSession.SessionType == SessionType.Practice || nextSession.SessionType == SessionType.Qualifying)
+            {
+                return;
+            }
+            // swap sessions
+            var tmp = session.SessionNr;
+            session.SessionNr = nextSession.SessionNr;
+            nextSession.SessionNr = tmp;
+            SortSessions();
         }
 
         public void SetModel(EventModel model)
@@ -198,6 +300,7 @@ namespace iRLeagueManager.Web.ViewModels
             _ = model ?? throw new ArgumentNullException(nameof(model));
             this.model = model;
             Sessions = new ObservableCollection<SessionViewModel>(model.Sessions.Select(x => new SessionViewModel(LoggerFactory, ApiService, x)));
+            SortSessions();
             OnPropertyChanged();
         }
 

8c52050e10bdeac1a85df16a6a1116e1e01e888a
Simon Schulze <simonschulze2589@hotmail.de>
2022-10-13 16:21:01 +0200

    Enable server side validation

---
+++ src/iRLeagueManager.Web/Data/StatusResult.cs
@@ -0,0 +1,53 @@
+﻿using iRLeagueApiCore.Common.Responses;
+using System.Linq;
+
+namespace iRLeagueManager.Web.Data
+{
+    public class StatusResult 
+    {
+        public StatusResult(bool success, string status)
+        {
+            IsSuccess = success;
+            Status = status;
+        }
+
+        public StatusResult(bool success, string status, string message) : 
+            this(success, status)
+        {
+            Message = message;
+        }
+
+        public StatusResult(bool success, string status, string message, IEnumerable<object> errors) : 
+            this(success, status, message)
+        {
+            Message = message;
+            Errors = errors;
+        }
+
+        public bool IsSuccess { get; }
+        public string Status { get; } = string.Empty;
+        public string Message { get; } = string.Empty;
+        public IEnumerable<object> Errors { get; } = Array.Empty<object>();
+        public IEnumerable<ValidationError> ValidationErrors => Errors.OfType<ValidationError>();
+
+        public const string Success = "Success";
+        public const string BadRequest = "Bad request";
+        public const string Unauthorized = "Unauthorized";
+        public const string ServerError = "Internal server Error";
+
+        public static StatusResult SuccessResult()
+        {
+            return new StatusResult(true, "Success");
+        }
+
+        public static StatusResult SuccessResult(string message)
+        {
+            return new StatusResult(true, "Success", message);
+        }
+
+        public static StatusResult FailedResult(string status, string message, IEnumerable<object> errors)
+        {
+            return new StatusResult(false, status, message, errors);
+        }
+    }
+}
---
+++ src/iRLeagueManager.Web/Shared/StatusResultValidator.cs
@@ -0,0 +1,96 @@
+﻿using iRLeagueManager.Web.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace iRLeagueManager.Web.Shared
+{
+    public class StatusResultValidator : ComponentBase
+    {
+        private ValidationMessageStore messageStore = default!;
+        [CascadingParameter]
+        private EditContext CurrentEditContext { get; set; } = default!;
+
+        public string ErrorMessage { get; set; } = string.Empty;
+
+        protected override void OnInitialized()
+        {
+            _ = CurrentEditContext ?? throw new InvalidOperationException($"Usage of {nameof(StatusResultValidator)} requires an {nameof(EditContext)} as cascading parameter");
+            messageStore = new ValidationMessageStore(CurrentEditContext);
+            CurrentEditContext.OnValidationRequested += (sender, args) => ClearErrors();
+        }
+
+        private void ClearErrors()
+        {
+            messageStore.Clear();
+            ErrorMessage = string.Empty;
+        }
+
+        public void ValidateResult(StatusResult result)
+        {
+            if (result.IsSuccess)
+            {
+                messageStore.Clear();
+                ErrorMessage = string.Empty;
+                return;
+            }
+
+
+            switch (result.Status)
+            {
+                case StatusResult.Unauthorized:
+                    AddUnauthorizedValidationMessages();
+                    break;
+                case StatusResult.BadRequest:
+                    AddBadRequestValidationMessages(result);
+                    break;
+                case StatusResult.ServerError:
+                    DisplayErrorMessage(result);
+                    break;
+                default:
+                    DisplayErrorMessage(result);
+                    break;
+            }
+        }
+
+        private void DisplayErrorMessage(StatusResult result)
+        {
+            foreach(var error in result.Errors)
+            {
+                if (error is Exception)
+                {
+                    ErrorMessage += $"\nError: {error.GetType()}";
+                    continue;
+                }
+                ErrorMessage += $"\nError: {error}";
+            }
+        }
+
+        private void AddUnauthorizedValidationMessages()
+        {
+            var usernameField = CurrentEditContext.Field("Username");
+            var passwordField = CurrentEditContext.Field("Password");
+            messageStore.Add(usernameField, "");
+            messageStore.Add(passwordField, "Incorrect Username or Password");
+        }
+
+        private void AddBadRequestValidationMessages(StatusResult result)
+        {
+            var validationErrors = result.ValidationErrors;
+            foreach (var validationError in validationErrors)
+            {
+                var fieldName = GetModelFieldName(validationError.Property);
+                var identifier = CurrentEditContext.Field(fieldName);
+                messageStore.Add(identifier, validationError.Error);
+            }
+        }
+
+        private static string GetModelFieldName(string requestFieldName)
+        {
+            if (requestFieldName.StartsWith("Model."))
+            {
+                return requestFieldName.Substring("Model.".Length);
+            }
+            return requestFieldName;
+        }
+    }
+}

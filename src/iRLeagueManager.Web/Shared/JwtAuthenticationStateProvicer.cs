130c295dc548f4d8b13d8ffc92f183dd0d978c3d
Simon Schulze <simonschulze2589@hotmail.de>
2022-10-14 17:19:40 +0200

    Add custom jwt authentication state provider

---
+++ src/iRLeagueManager.Web/Shared/JwtAuthenticationStateProvicer.cs
@@ -0,0 +1,47 @@
+﻿using iRLeagueApiCore.Client.Http;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace iRLeagueManager.Web.Shared
+{
+    public class JwtAuthenticationStateProvicer : AuthenticationStateProvider
+    {
+        private readonly JwtSecurityTokenHandler tokenHandler = new();
+        private readonly ITokenStore tokenStore;
+        private string lastToken = string.Empty;
+
+        public JwtAuthenticationStateProvicer(ITokenStore tokenStore)
+        {
+            this.tokenStore = tokenStore;
+        }
+
+        private async Task<ClaimsPrincipal> GetTokenUser()
+        {
+            var token = await tokenStore.GetTokenAsync();
+            if (string.IsNullOrEmpty(token))
+            {
+                return GetAnonymous();
+            }
+            var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+            var identity = new ClaimsIdentity(jwtSecurityToken.Claims);
+            return new ClaimsPrincipal(identity);
+        }
+
+        private static ClaimsPrincipal GetAnonymous()
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Sid, "0"),
+                new Claim(ClaimTypes.Name, "Anonymous"),
+                new Claim(ClaimTypes.Role, "Anonymous")
+            }, null));
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var user = await GetTokenUser();
+            return new AuthenticationState(user);
+        }
+    }
+}

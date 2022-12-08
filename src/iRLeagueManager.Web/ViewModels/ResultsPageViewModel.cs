b2b523c449205310126410ae21e5c9a69d7863c7
Simon Schulze <simonschulze2589@hotmail.de>
2022-12-08 16:45:13 +0100

    Add calculate button for results

--- src/iRLeagueManager.Web/ViewModels/ResultsPageViewModel.cs
+++ src/iRLeagueManager.Web/ViewModels/ResultsPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using iRLeagueApiCore.Common.Models;
 using iRLeagueManager.Web.Data;
+using iRLeagueManager.Web.Extensions;
 using System.Collections.ObjectModel;
 using System.Linq;
 
@@ -108,6 +109,41 @@ namespace iRLeagueManager.Web.ViewModels
                 {
                     SelectedResultIndex = Results.Count;
                 }
+            }
+            finally
+            {
+                Loading = false;
+            }
+        }
+
+        public async Task<StatusResult> TriggerCalculation(CancellationToken cancellationToken = default)
+        {
+            if (ApiService.CurrentLeague is null)
+            {
+                return LeagueNullResult();
+            }
+
+            if (SelectedEvent is null)
+            {
+                return new StatusResult(false, "No event selected");
+            }
+            try
+            {
+                Loading = true;
+                var request = ApiService.CurrentLeague.Events()
+                .WithId(SelectedEvent.EventId)
+                .Results()
+                .Calculate()
+                .Post(cancellationToken);
+                var result = await request;
+
+                if (result.Success)
+                {
+                    await Task.Delay(2000);
+                    await LoadFromEventAsync(SelectedEvent.EventId);
+                }
+
+                return result.ToStatusResult();
             }
             finally
             {

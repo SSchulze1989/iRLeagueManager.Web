4230656c736294c63abc446d652d5e78ace182e6
Simon Schulze <simonschulze2589@hotmail.de>
2023-07-02 02:39:52 +0200

    Add modal for editing bonus points

---
+++ src/iRLeagueManager.Web/Data/BonusPointConfig.cs
@@ -0,0 +1,49 @@
+﻿namespace iRLeagueManager.Web.Data;
+
+public class BonusPointConfig
+{
+    public BonusPointOption Option { get; set; }
+    public char OptionId
+    {
+        get => Option.Id;
+        set => Option = BonusPointOption.Available.FirstOrDefault(x => x.Id == value) ?? BonusPointOption.Available.First();
+    }
+    public int Position { get; set; }
+    public int Points { get; set; }
+
+    public BonusPointConfig()
+    {
+        Option = BonusPointOption.Available.First();
+    }
+
+    public BonusPointConfig(BonusPointOption option, int position, int points)
+    {
+        Option = option;
+        Position = position;
+        Points = points;
+    }
+
+    public BonusPointConfig(string bonusKey, int points)
+    {
+        if (string.IsNullOrEmpty(bonusKey))
+        {
+            throw new ArgumentException(nameof(bonusKey));
+        }
+        var bonusKeyId = bonusKey[0];
+        int bonusKeyValue = 0;
+        if (bonusKey.Length > 1 && int.TryParse(bonusKey[1..], out bonusKeyValue) == false)
+        {
+            throw new ArgumentException(nameof(bonusKey));
+        }
+        var bonusOption = BonusPointOption.Available.FirstOrDefault(x => x.Id == bonusKeyId)
+            ?? throw new ArgumentException(nameof(bonusKey));
+        Option = bonusOption;
+        Position = bonusKeyValue;
+        Points = points;
+    }
+
+    public override string ToString()
+    {
+        return $"{Option.Description} {(Position != 0 ? Position : "")} => {Points}";
+    }
+}
---
+++ src/iRLeagueManager.Web/Data/BonusPointOption.cs
@@ -0,0 +1,23 @@
+﻿namespace iRLeagueManager.Web.Data;
+
+public class BonusPointOption
+{
+    public char Id { get; set; }
+    public string Description { get; set; }
+    public bool HasPosition { get; set; }
+
+    public BonusPointOption(char id, string description, bool hasPosition)
+    {
+        Id = id;
+        Description = description;
+        HasPosition = hasPosition;
+    }
+
+    public static BonusPointOption[] Available = new BonusPointOption[]
+    {
+        new('p', "Position", true),
+        new('q', "Qualy Position", true),
+        new('f', "Fastest Lap", false),
+        new('c', "Cleanest Driver", false),
+    };
+}

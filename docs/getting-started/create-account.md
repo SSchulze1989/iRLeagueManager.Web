a27f053efeece37cebfe76eae98fac963bb02531
Simon Schulze <simonschulze2589@hotmail.de>
2023-09-16 00:37:22 +0200

    Update frontend elements - Update members route - Add User login and profile menu

--- docs/getting-started/create-account.md
+++ docs/getting-started/create-account.md
@@ -4,7 +4,7 @@
 
 Before you can host your league in iRLeagueManager you need to set-up a personal user account.  
 
-[Click here to open sign-up page](https://irleaguemanager.net/member/register)
+[Click here to open sign-up page](https://irleaguemanager.net/members/register)
 
 Follow the steps to create and verify your account.
-Once you finished with the verification you can sign-in through the [login page](https://irleaguemanager.net/member/login)
+Once you finished with the verification you can sign-in through the [login page](https://irleaguemanager.net/members/login)
---
+++ src/iRLeagueManager.Web/Shared/UtilityComponentBase.cs
@@ -0,0 +1,164 @@
+﻿using iRLeagueApiCore.Common;
+using iRLeagueManager.Web.Data;
+using iRLeagueManager.Web.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+using MvvmBlazor.Components;
+using System.Net;
+using System.Threading;
+
+namespace iRLeagueManager.Web.Shared;
+
+/// <summary>
+/// Base class for components that require access to enhanced utility functions but do not need the overhead of initializing a full <see cref="LeagueComponentBase"/>
+/// </summary>
+public class UtilityComponentBase : MvvmComponentBase
+{
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+    [Inject]
+    protected IJSRuntime JsRuntime { get; set; } = default!;
+    [Inject]
+    protected SharedStateService Shared { get; set; } = default!;
+
+    private readonly CancellationTokenSource cancellationTokenSource = new();
+    protected CancellationToken CancellationToken => cancellationTokenSource.Token;
+
+    private IDisposable? locationChangingHandler;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        NavigationManager.LocationChanged += OnLocationChanged;
+        Shared.StateChanged += SharedStateChanged;
+    }
+
+    protected virtual void SharedStateChanged(object? sender, EventArgs e)
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing == false)
+        {
+            locationChangingHandler?.Dispose();
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+            Shared.StateChanged -= SharedStateChanged;
+            NavigationManager.LocationChanged -= OnLocationChanged;
+        }
+
+        base.Dispose(disposing);
+    }
+
+    public string GetRoleString(string? leagueName, params string[] roleNames)
+    {
+        IEnumerable<string> roles = new[] { "Admin" };
+        if (string.IsNullOrEmpty(leagueName) == false)
+        {
+            var roleValues = roleNames.Select(x => LeagueRoles.GetRoleValue(x));
+            roleValues = roleValues.Concat(roleValues.SelectMany(LeagueRoles.ImplicitRoleOf)).Distinct();
+            var leagueRoleNames = roleValues
+                .Select(x => LeagueRoles.GetLeagueRoleName(leagueName, x))
+                .NotNull();
+            roles = roles.Concat(leagueRoleNames);
+        }
+        return string.Join(',', roles);
+    }
+
+    protected async Task ScrollToElementId(string id)
+    {
+        await JsRuntime.InvokeVoidAsync("scrollToElementId", id);
+    }
+
+    protected async Task ScrollToElement(ElementReference reference)
+    {
+        await ScrollToElementId(reference.Id);
+    }
+
+    protected async Task EnableTooltips()
+    {
+        await JsRuntime.InvokeVoidAsync("enableTooltips", "right");
+    }
+
+    protected void NavigateTo(string url, bool replace = false, string? returnUrl = null)
+    {
+        if (string.IsNullOrEmpty(returnUrl) == false)
+        {
+            var queryParameters = new Dictionary<string, object?> { { "returnUrl", returnUrl } };
+            url = NavigationManager.GetUriWithQueryParameters(url, queryParameters);
+        }
+        NavigationManager.NavigateTo(url, replace: replace);
+    }
+
+    protected void NavigateToRelative(string relativeUrl, bool replace = false)
+    {
+        var absoluteUrl = NavigationManager.ToAbsoluteUri(relativeUrl).ToString();
+        NavigateTo(absoluteUrl, replace: replace);
+    }
+
+    protected void ForceNavigateTo(string url, bool fullReload = false)
+    {
+        if (fullReload)
+        {
+            NavigationManager.NavigateTo(url, forceLoad: true);
+            return;
+        }
+
+        NavigationManager.NavigateTo("/");
+        NavigationManager.NavigateTo(url);
+    }
+
+    /// <summary>
+    /// Activate tracking for location changing.
+    /// Override <see cref="OnLocationChanging(LocationChangingContext)"/> to perform an action when the location changes
+    /// </summary>
+    protected void RegisterLocationChangingHandler()
+    {
+        locationChangingHandler = NavigationManager.RegisterLocationChangingHandler(OnLocationChanging);
+    }
+
+    /// <summary>
+    /// Perform action when location is changing (e.g. cancel location on certain conditions)
+    /// Requires call to <see cref="RegisterLocationChangingHandler()"/> once to initialize location change tracking
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    protected virtual async ValueTask OnLocationChanging(LocationChangingContext context)
+    {
+        await Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Perform action after location has changed
+    /// </summary>
+    /// <param name="e"></param>
+    protected virtual void OnLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+    }
+
+    /// <summary>
+    /// Get the current url as HTML encoded
+    /// </summary>
+    /// <returns></returns>
+    protected string GetCurrentUrlEncoded()
+    {
+        return WebUtility.UrlEncode(new Uri(NavigationManager.Uri).PathAndQuery);
+    }
+
+    /// <summary>
+    /// Get the url from returnUrl= parameter. If returnUrl is not set -> return the current url as HTML encoded instead
+    /// </summary>
+    /// <returns></returns>
+    protected string GetReturnUrl()
+    {
+        var returnUrl = NavigationManager.QueryParameter<string>("returnUrl");
+        if (string.IsNullOrEmpty(returnUrl))
+        {
+            return GetCurrentUrlEncoded();
+        }
+        return WebUtility.UrlEncode(returnUrl);
+    }
+}

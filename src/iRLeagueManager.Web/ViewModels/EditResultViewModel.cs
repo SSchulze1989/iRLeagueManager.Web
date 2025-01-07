6ca8a721d6878929de968357c3c26cb84ffe43ea
Simon Schulze <simonschulze2589@hotmail.de>
2025-01-07 22:32:49 +0100

    Add edit Result table page
    * Add view models
    * Add components

---
+++ src/iRLeagueManager.Web/ViewModels/EditResultViewModel.cs
@@ -0,0 +1,115 @@
+﻿using iRLeagueApiCore.Common.Models;
+using iRLeagueManager.Web.Data;
+using iRLeagueManager.Web.Extensions;
+
+namespace iRLeagueManager.Web.ViewModels;
+
+public sealed class EditResultViewModel : LeagueViewModelBase<EditResultViewModel, RawEventResultModel>
+{
+    public EditResultViewModel(ILoggerFactory loggerFactory, LeagueApiService apiService)
+        : base(loggerFactory, apiService, new())
+    {
+    }
+
+    private List<TeamInfoModel> teams = [];
+    public List<TeamInfoModel> Teams { get => teams; set => Set(ref teams, value); }
+
+    private List<MemberInfoModel> members = [];
+    public List<MemberInfoModel> Members { get => members; set => Set(ref members, value); }
+
+    private List<RawSessionResultViewModel> sessionResults = [];
+    public List<RawSessionResultViewModel> SessionResults { get => sessionResults; set => Set(ref sessionResults, value); }
+
+    public async Task<StatusResult> Load(long eventId, CancellationToken cancellationToken = default)
+    {
+        if (CurrentLeague is null)
+        {
+            return LeagueNullResult();
+        }
+
+        try
+        {
+            Loading = true;
+            var request = CurrentLeague
+                .Events()
+                .WithId(eventId)
+                .Results()
+                .Raw();
+            var result = await request.Get(cancellationToken);
+            if (!result.Success || result.Content is not RawEventResultModel model)
+            {
+                return result.ToStatusResult();
+            }
+            SetModel(model);
+
+            var loadMembers = await CurrentLeague
+                .Members()
+                .Get(cancellationToken);
+            if (!loadMembers.Success || loadMembers.Content is null)
+            {
+                return loadMembers.ToStatusResult();
+            }
+            Members = loadMembers.Content.Cast<MemberInfoModel>().ToList();
+
+            var loadTeams = await CurrentLeague
+                .Teams()
+                .Get(cancellationToken);
+            if (!loadTeams.Success || loadTeams.Content is null)
+            {
+                return loadTeams.ToStatusResult();
+            }
+            Teams = loadTeams.Content
+                .Select(team => new TeamInfoModel()
+                {
+                    TeamId = team.TeamId,
+                    Name = team.Name,
+                    TeamColor = team.TeamColor,
+                })
+                .ToList();
+
+            return StatusResult.SuccessResult();
+        }
+        finally
+        {
+            Loading = false;
+        }
+    }
+
+    public async Task<StatusResult> SaveAsync(CancellationToken cancellationToken)
+    {
+        if (CurrentLeague is null)
+        {
+            return LeagueNullResult();
+        }
+
+        try
+        {
+            Loading = true;
+            var result = await CurrentLeague.Events()
+                .WithId(model.EventId)
+                .Results()
+                .Raw()
+                .Put(model, cancellationToken);
+            if (result.Success && result.Content is not null)
+            {
+                SetModel(result.Content);
+            }
+            return result.ToStatusResult();
+        }
+        finally
+        {
+            Loading = false;
+        }
+    }
+
+    protected override void SetModel(RawEventResultModel model)
+    {
+        base.SetModel(model);
+        SessionResults = model.SessionResults
+            .Select(x => new RawSessionResultViewModel(LoggerFactory, ApiService, x)
+            {
+                ParentViewModel = this,
+            })
+            .ToList();
+    }
+}
---
+++ src/iRLeagueManager.Web/ViewModels/RawSessionResultViewModel.cs
@@ -0,0 +1,25 @@
+﻿using iRLeagueApiCore.Common.Models;
+using iRLeagueManager.Web.Data;
+
+namespace iRLeagueManager.Web.ViewModels;
+
+public sealed class RawSessionResultViewModel : LeagueViewModelBase<RawSessionResultViewModel, RawSessionResultModel>
+{
+    public RawSessionResultViewModel(ILoggerFactory loggerFactory, LeagueApiService apiService, RawSessionResultModel model) 
+        : base(loggerFactory, apiService, model)
+    {
+    }
+
+    public long SessionId { get => model.SessionId; }
+
+    private List<RawResultRowViewModel> resultRows = [];
+    public List<RawResultRowViewModel> ResultRows { get => resultRows; set => Set(ref resultRows, value); }
+
+    protected override void SetModel(RawSessionResultModel model)
+    {
+        base.SetModel(model);
+        ResultRows = model.ResultRows
+            .Select(x => new RawResultRowViewModel(LoggerFactory, ApiService, x))
+            .ToList();
+    }
+}
